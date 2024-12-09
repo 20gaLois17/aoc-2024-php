4604c17704646aaf6258dbed5283845f5cc57ae0
Sebastian Koletzko <sk@bitsandlikes.de>
2024-12-09 21:56:02 +0100

    day-7 part two

--- 2024/day-7/solution.php
+++ 2024/day-7/solution.php
@@ -2,6 +2,7 @@
 enum Operations {
     case Add;
     case Mul;
+    case Conc;
 }
 
 class Solution extends AdventOfCode\Solution
@@ -31,30 +32,53 @@ class Solution extends AdventOfCode\Solution
     {
         $input = $this->input->load();
         $result = 0;
+        foreach ($input as $line) {
+            $split = explode(": ", $line);
+            $target = $split[0];
+            $operands = explode(" ", $split[1]);
 
+            $count1 = 0;
+            $count2 = 0;
+            $count3 = 0;
+            $this->backtrack($target, $operands, [Operations::Add], $count1, true);
+            $this->backtrack($target, $operands, [Operations::Mul], $count2, true);
+            $this->backtrack($target, $operands, [Operations::Conc], $count3, true);
+
+            if ($count1+$count2+$count3 > 0) {
+                $result += $target;
+            }
+        }
         return $result;
     }
 
-    public function backtrack(int $target, array $operands, array $operations, int &$solutionCount): void
+    private function backtrack(int $target, array $operands, array $operations, int &$solutionCount, bool $partTwo = false): void
     {
         if (sizeof($operands)-1 === sizeof($operations)) {
             if ($this->getResult($operations, $operands) === $target) {
                 $solutionCount++;
+                return;
             }
         }
 
-        if (sizeof($operands) > 0) {
-            if ($this->getResult($operations, $operands) >= $target) {
-                if (($operations[sizeof($operations)-1]) === Operations::Add) {
-                    return;
-                }
-            }
-        }
+        // if (sizeof($operands) > 0) {
+        //     if ($this->getResult($operations, $operands) >= $target) {
+        //         $lastOperation = $operations[sizeof($operations)-1];
+        //         if (!$partTwo) {
+        //             if ($lastOperation === Operations::Add) {
+        //                 return;
+        //             }
+        //         } else {
+        //             if ($lastOperation === Operations::Conc) {
+        //                 return;
+        //             }
+        //         }
+        //     }
+        // }
 
         $ops = $this->down($operands, $operations);
         while ($ops !== null) {
-            $this->backtrack($target, $operands, $ops, $solutionCount);
-            $ops = $this->next($ops);
+            $this->backtrack($target, $operands, $ops, $solutionCount, $partTwo);
+            $ops = $this->next($ops, $partTwo);
         }
     }
 
@@ -67,11 +91,15 @@ class Solution extends AdventOfCode\Solution
         return $operations;
     }
 
-    private function next(array $operations): ?array
+    private function next(array $operations, $partTwo = false): ?array
     {
         $operation = array_pop($operations);
         if ($operation === Operations::Mul) {
             $operations[] = Operations::Add;
+            return $operations;
+        }
+        if ($operation === Operations::Add && $partTwo) {
+            $operations[] = Operations::Conc;
             return $operations;
         }
         return null;
@@ -86,13 +114,17 @@ class Solution extends AdventOfCode\Solution
         return $result;
     }
 
-    private function applyOperation($arg1, $arg2, Operations $operation) {
+    private function applyOperation($arg1, $arg2, Operations $operation): int
+    {
         switch ($operation) {
             case Operations::Add:
                 return (int)$arg1+(int)$arg2;
 
             case Operations::Mul:
                 return (int)$arg1*(int)$arg2;
+
+            case Operations::Conc:
+                return (int)((string)$arg1 . (string)$arg2);
         }
     }
 }
